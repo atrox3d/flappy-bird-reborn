398282072fe8623b0532f57ef8cb02bb54a6489d
atrox3d <atrox3d@gmail.com>
2016-01-04 17:00:50 +0100

    Implement title screen

--- game/states/menu.js
+++ game/states/menu.js
@@ -18,6 +18,16 @@ var FlappyBird;
             this.background = this.game.add.sprite(0, 0, 'background');
             this.ground = this.game.add.tileSprite(0, 400, 335, 112, 'ground');
             this.ground.autoScroll(-200, 0);
+            this.titleGroup = this.game.add.group();
+            this.title = this.game.add.sprite(0, 0, 'title');
+            this.titleGroup.add(this.title);
+            this.bird = this.game.add.sprite(200, 5, 'bird');
+            this.titleGroup.add(this.bird);
+            this.bird.animations.add('flap');
+            this.bird.animations.play('flap', 12, true);
+            this.titleGroup.x = 30;
+            this.titleGroup.y = 0;
+            this.game.add.tween(this.titleGroup).to({ y: 15 }, 350, Phaser.Easing.Linear.None, true, 0, 1000, true);
         };
         Menu.prototype.update = function () {
         };
